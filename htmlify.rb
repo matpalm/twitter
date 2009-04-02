e4ca75c280bb04ce87ab24d7df1ce174e1409271
mat <matthew.kelcey@gmail.com>
2009-04-02 21:21:55 +1100

    include header in table out

--- htmlify.rb
+++ htmlify.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 every_second = false
+puts "<tr><td>user</td><td>#followers</td><td>#following</td><td>popularity</td></tr>"
 STDIN.each do |line|
 		screen_name, num_following, num_followers, popularity	= line.chomp.split 
     td = every_second ? '<td>' : '<td class="y">'
-    even_second = !even_second
+    every_second = !every_second
     print "<tr>"
     print "#{td}#{screen_name}</td>"
     print "#{td}#{num_followers}</td>"

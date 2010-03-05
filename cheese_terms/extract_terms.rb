c8ee22c726d14ac1819af2b34e2f3335066e9340
mat <matthew.kelcey@gmail.com>
2010-03-06 10:12:32 +1100

    simpler

--- cheese_terms/extract_terms.rb
+++ cheese_terms/extract_terms.rb
@@ -12,16 +12,18 @@ class String
 		begin
 			url = "http://search.yahooapis.com/ContentAnalysisService/V1/termExtraction"
 			
-			text = Curl::PostField.content('context',self)
-			query = Curl::PostField.content('query','cheese')
-			output = Curl::PostField.content('output','json')
-			api_key = Curl::PostField.content('appid',ENV['APPID'])
+			params = [
+				Curl::PostField.content('context',self),
+				Curl::PostField.content('query','cheese'),
+				Curl::PostField.content('output','json'),
+				Curl::PostField.content('appid',ENV['APPID']) 
+			]
 
-			resp = Curl::Easy.http_post(url, text, query, output, api_key).body_str
+			resp = Curl::Easy.http_post(url, *params).body_str
 
 			JSON::parse(resp)['ResultSet']['Result']
 		rescue 
-			STDERR.puts "error json parsing response #{resp}, rate limit exceeded?"
+			STDERR.puts "#{Time.now} error json parsing response [#{resp}] for #{self}, rate limit exceeded?"
 			sleep 60
 			retry
 		end		

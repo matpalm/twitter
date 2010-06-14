1c31be05badbf148aee3d5aaf828d1dbf5ec5938
mat <matthew.kelcey@gmail.com>
2010-06-15 08:16:08 +1000

    use yield for easier reuse

--- searchslurp/update_existing_documents.rb
+++ searchslurp/update_existing_documents.rb
@@ -5,20 +5,31 @@
 require 'init_mongo.rb'
 require 'date'
 require 'time'
+
+def for_each_in clause
+  col = connect_to_mongo
+  num_updated = 0
+  col.find(clause).each do |tweet|
+    yield tweet
+    col.save(tweet)
+    num_updated += 1
+  end
+  puts "updated #{num_updated.commaify} of #{col.count.commaify} documents"
+end
+
+for_each_in({'profile_image_url'=>{'$ne'=>nil}}) do |tweet|
+  tweet.delete 'profile_image_url'
+end
+
+=begin
 require 'sanitise.rb'
-
 sanitiser = Sanitiser.new(:duplicate_spaces_removed)
-
-num_updated = 0
-
-col = connect_to_mongo
-col.find('sanitised_text'=>nil).each do |tweet|
+for_each_in({'sanitised_text'=>nil}) do |tweet|
   text = tweet['text']
   sanitised_text = sanitiser.sanitise(text)
   tweet['sanitised_text'] = sanitised_text
-  col.save(tweet)
-  num_updated += 1
 end
+=end
 
-puts "updated #{num_updated.commaify} of #{col.count.commaify} documents"
+
 
